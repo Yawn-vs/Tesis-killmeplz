3fba3610f1bf19ca46790dbc5975f5b72929a8aa
Yawn-vs <yandyvist@gmail.com>
2020-07-02 20:34:26 -0400

    mensaje lol

--- Proyecto/app/src/main/java/com/example/proyecto/tab2.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/tab2.kt
@@ -1,13 +1,22 @@
 package com.example.tesisapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tesisapp.NextActivity
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class tab2 : AppCompatActivity() {
 
     /*private TabLayout tabLayout
     private ViewPager viewPager
@@ -19,12 +28,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val btnShow = findViewById<Button>(R.id.btnShow)
-        //btnShow?.setOnClickListener { Toast.makeText(this@MainActivity, R.string.welcome_message, Toast.LENGTH_LONG).show() }
-        botonRegistro.setOnClickListener(){
-            val intentGoTotabtab1= Intent (this, tab1::class.java)
-            startActivity(intentGoTotabtab1)
-        }
+       // tablayout.setOnTabSelectedListener(new tablayout.on)
     }
 
     fun moveNextPage(){
